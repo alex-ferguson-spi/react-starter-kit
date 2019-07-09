aa7808645c578d4d574dd99eb59f575282c05209
alex-ferguson-spi <alex.ferguson@digilock.com>
2019-07-09 16:37:40 -0700

    Add adv webpack tools such as source mapping

--- utils/build/webpack.dev.js
+++ utils/build/webpack.dev.js
@@ -1,43 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
-// empties previous builds when creating new ones
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
+  alias: {
+    "react-dom": "@hot-loader/react-dom"
+  },
   mode: "development",
-  entry: "./src/index.js",
   devtool: "eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 3000,
-    hot: true
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Webpack React Kit",
-      template: "./src/index.html"
+    new Dotenv({
+      path: "./.env/.development"
     })
   ]
 };
--- utils/build/webpack.prod.js
+++ utils/build/webpack.prod.js
@@ -1,43 +1,11 @@
-const path = require("path");
-const webpack = require("webpack");
-// empties previous builds when creating new ones
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.js",
   devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 3000,
-    hot: true
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Webpack React Kit",
-      template: "./src/index.html"
+    new Dotenv({
+      path: "./.env/.production"
     })
   ]
 };
